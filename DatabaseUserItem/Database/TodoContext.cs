7031d31b5d28e2dbfd2683c05c8d13e4bc41a4d2
AnonimAnonim2245 <98339220+AnonimAnonim2245@users.noreply.github.com>
2023-09-15 22:40:44 +0300

    Add project files.

---
+++ DatabaseUserItem/Database/TodoContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DatabaseUserItem.Models;
+
+namespace DatabaseUserItem.Database
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; } = null!;
+        public DbSet<User> Users { get; set; } = null!;
+    }
+}
---
+++ DatabaseUserItem/Program.cs
@@ -0,0 +1,35 @@
+using DatabaseUserItem.Database;
+using Microsoft.EntityFrameworkCore;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+//var provider = builder.Services.BuildServiceProvider();
+//var configuration = provider.GetService<IConfiguration>();
+
+builder.Services.AddControllers();
+
+//builder.Services.AddDbContext<TodoContext>(item => item.UseSqlServer(configuration.GetConnectionString("mycoon")));
+builder.Services.AddDbContext<TodoContext>(opt =>
+    opt.UseSqlServer(builder.Configuration.GetSection("ConnectionString").Value));
+
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
